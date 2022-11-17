b4795ecec814b385c1fc66376b18dc12fc7a79f7
Jennifer Chan <jenniferchan2267@yahoo.com>
2022-11-16 19:04:19 -0800

    add eventlistener

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ const decreaseBtn = document.getElementById('decrease');
 const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
+let size = 10;
+let isPressed = false;
+colorEl.value = 'black';
+let color = colorEl.value;
+let x;
+let y;
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+document.addEventListener('mouseup', (e) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
